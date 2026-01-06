4eaefc750d2252f73a8abc46a5e325681ed4ce4d
Mahi438 <jmahi5210@gmail.com>
2026-01-06 10:47:21 +0530

    Updated

--- src/api.js
+++ src/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://product-backend-yq8l.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
   return req;
 });
 
